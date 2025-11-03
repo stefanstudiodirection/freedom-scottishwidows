60839c6a28f18cb89ca43bc749f1bc1e3e3e830d
stefanstudiodirection <stefan.radovic@studiodirection.com>
2025-11-03 13:52:07 +0100

    default light tema

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
 	<QueryClientProvider client={queryClient}>
-		<ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+		<ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
 			<TooltipProvider>
 				<AccountProvider>
 					<Toaster />
